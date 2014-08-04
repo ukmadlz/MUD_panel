0af0135e601c063c4cc465c1c93ddf7733ec1c5e
Mike Elsmore <mike@elsmore.me>
2014-08-04 13:59:03 +0100

    Change the Pusher JS to the Sandbox and repoint the controller to Webface

--- app/views/single_map.blade.php
+++ app/views/single_map.blade.php
@@ -5,13 +5,13 @@
 		{{ $html }}
 	</div>
 	<div class="col-md-6">
-		<iframe src="http://104.131.212.45/webface/" width="100%" height="600px">
-			
+		<iframe src="http://leedshack.curtish.me/webface/" width="100%" height="600px">
+
 		</iframe>
 	</div>
 </div>
 
-@show 
+@show
 
 @section('head-area')
 <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
@@ -20,23 +20,23 @@
 @section('javascript-include')
 <script src="//js.pusher.com/2.2/pusher.min.js" type="text/javascript"></script>
 <script>
-	var pusher = new Pusher('56e8164e0555e60345c9');
+	var pusher = new Pusher(<?=getenv("PUSHER_KEY");?>);
 	var channel = pusher.subscribe('map_{{ $game_id }}');
     channel.bind('move', function(data) {
 		console.log(data);
 		$('.player_' + data.pid).remove();
 		$('.pos_' + data.coord).html('<i class="fa ' + data.icon + ' player_' + data.pid + '" title="' + data.pid + '"></i>');
 	});
-	
+
 	channel.bind('loot', function(data) {
 		 console.log('User: ' + data.pid + " Loot: " + data.loot);
 	});
-	
+
 	channel.bind('dead', function(data) {
 		console.log('User: ' + data.pid + " Died (You: " + data.your_l + " v Them: " + data.there_l + " data: " + data.data);
 		$('.player_' + data.pid).remove();
 	});
-	
+
 	channel.bind('beat', function(data) {
 		 console.log('User: ' + data.pid + " Beat a monster");
 	});
